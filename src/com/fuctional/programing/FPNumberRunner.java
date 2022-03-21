330235beadfbdfdf6d646b0d677498f5c2c5ad94
zenjutahi <zenjutahi@gmail.com>
2022-03-21 13:51:59 +0300

    Functional programming - understanding FP

--- src/com/fuctional/programing/FPNumberRunner.java
+++ src/com/fuctional/programing/FPNumberRunner.java
@@ -1,6 +1,8 @@
 package com.fuctional.programing;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FPNumberRunner {
@@ -22,9 +24,18 @@ public class FPNumberRunner {
         // print the length of each element
         myList.stream().map(e -> e.length()).forEach(e -> System.out.println(e));
 
+        // terminal operation max
+        int ans = List.of(12,12,67,23,44).stream().max((n1, n2) -> Integer.compare(n1,n2)).get();
+        System.out.println(ans);
+        // Collecting to List -terminal operation
+        List result = List.of(12,12,67,23,44).stream().filter(e -> e%2==0).collect(Collectors.toList());
+        System.out.println(result);
+        // Print a list of squares of the first 10 numbers
+        List values = IntStream.range(1,11).map(e -> e * e).boxed().collect(Collectors.toList());
+        System.out.println(values);
     }
 
-    // Sum with filtering FP i.e has no variable mutation
+    // Sum with filtering FP i.e. has no variable mutation
     private static void funcProgrammingSum(List<Integer> numbers) {
         int sum = numbers.stream().filter(number -> number%2!=0)
                 .reduce(0, (number1, number2) -> number1 + number2);
