2dba8c43279b3bced233d42d7d4ed1f78cc38133
zenjutahi <zenjutahi@gmail.com>
2022-03-21 12:02:15 +0300

    Generics - creating a CumtomList class that accepts all types

--- src/com/learn/generics/GenericsRunner.java
+++ src/com/learn/generics/GenericsRunner.java
@@ -6,15 +6,17 @@ public class GenericsRunner {
 
         list.addElement("This1");
         list.addElement("This2");
+        String value = list.get(0);
 
-        System.out.println(list);
+        System.out.println(value);
 
         MyCustomList<Integer> list2 = new MyCustomList();
 
         list2.addElement(Integer.valueOf(7));
         list2.addElement(Integer.valueOf(8));
+        Integer number = list2.get(0);
 
-        System.out.println(list2);
+        System.out.println(number);
 
     }
 }
--- src/com/learn/generics/MyCustomList.java
+++ src/com/learn/generics/MyCustomList.java
@@ -18,4 +18,8 @@ public class MyCustomList<T> {
     public  String toString(){
         return list.toString();
     }
+
+    public T get(int index) {
+        return list.get(index);
+    }
 }
