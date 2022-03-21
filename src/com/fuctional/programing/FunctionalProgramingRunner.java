a810b44d17d6e6ccce3aadea972779d80bbb7551
zenjutahi <zenjutahi@gmail.com>
2022-03-21 12:36:02 +0300

    Functional programming - understanding FP

--- src/com/fuctional/programing/FunctionalProgramingRunner.java
+++ src/com/fuctional/programing/FunctionalProgramingRunner.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 public class FunctionalProgramingRunner {
     public static void main(String[] args){
-        List<String> list = List.of("cow", "man", "apple");
+        List<String> list = List.of("cow", "man","pan","can","apple");
 
         printBasic(list);
         printWithFP(list);
+
+        printBasicWithFilter(list);
+        printFilersWithFP(list);
     }
 
     // Normal or basic way to loop and print elements in a list
@@ -22,7 +25,24 @@ public class FunctionalProgramingRunner {
     // Doing the same using functional programing
     private static void printWithFP(List<String> list) {
         list.stream().forEach(
-                element -> System.out.println(element)
+                element -> System.out.println("Element - " + element)
+        );
+    }
+
+    // Normal or basic way to loop and print elements in a list with a filter
+    private static void printBasicWithFilter(List<String> list) {
+        for(String element: list){
+            if(element.endsWith("an")){
+                System.out.println(element);
+            }
+        }
+    }
+
+    // Doing the same using functional programing
+    private static void printFilersWithFP(List<String> list) {
+        list.stream()
+                .filter(element -> element.endsWith("an"))
+                .forEach(element -> System.out.println("Element - " + element)
         );
     }
 }
