d365f70551ea254a7bc5a582e3affb94620a6a6e
zenjutahi <zenjutahi@gmail.com>
2022-03-21 14:28:31 +0300

    Functional programming - Understanding functional interface

--- src/com/fuctional/programing/LambdaBehindTheScreensRunner.java
+++ src/com/fuctional/programing/LambdaBehindTheScreensRunner.java
@@ -2,6 +2,7 @@ package com.fuctional.programing;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 
@@ -23,16 +24,25 @@ class EvenNumberConsumer implements Consumer<Integer> {
     }
 }
 
+class CustomMultiplierFunction implements Function<Integer, Integer> {
+
+    @Override
+    public Integer apply(Integer number) {
+        return number*number;
+    }
+}
+
 public class LambdaBehindTheScreensRunner {
     public static void main(String[] args){
         List.of(12,13,56,24,33,17).stream()
                 .filter(n -> n%2==0)
+                .map(n -> n * n)
                 .forEach(e -> System.out.println(e));
 
         List.of(12,13,56,24,33,17).stream()
                 .filter(new EvenNumberPredicate())
+                .map(new CustomMultiplierFunction())
                 .forEach(new EvenNumberConsumer());
-
 
     }
 }
