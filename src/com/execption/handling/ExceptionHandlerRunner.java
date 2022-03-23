4ff7b53960910027433e805ad79fec703763fb10
zenjutahi <zenjutahi@gmail.com>
2022-03-23 16:32:51 +0300

    Exception handling

--- src/com/execption/handling/ExceptionHandlerRunner.java
+++ src/com/execption/handling/ExceptionHandlerRunner.java
@@ -1,13 +1,24 @@
 package com.execption.handling;
 
 public class ExceptionHandlerRunner {
+
     public static void main(String[] args) {
-        method1();
-        System.out.println("Print me");
+            method1();
+            System.out.println("Main Ended");
     }
 
     private static void method1() {
-        String str = null;
-        str.length();
+        method2();
+        System.out.println("Method1 Ended");
+    }
+
+    private static void method2() {
+        try {
+            String str = null;
+            str.length();
+            System.out.println("Method2 Ended");
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
