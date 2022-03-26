dbdda70f8c6abca9023161d01182cfa4b25c0e06
zenjutahi <zenjutahi@gmail.com>
2022-03-26 17:13:24 +0300

    Java access modifiers tips

--- src/com/java/tips/StaticModifierRunner.java
+++ src/com/java/tips/StaticModifierRunner.java
@@ -4,6 +4,8 @@ package com.java.tips;
 
 class Player{
     private  String name;
+
+    // Use Static variable When you want to have a constant value between Object instances
     private static int count;
 
     public Player(String name){
@@ -11,6 +13,7 @@ class Player{
         count++;
     }
 
+    // Static method used when its meant foe a static variable
     public static int getCount() {
         return count;
     }
@@ -20,8 +23,8 @@ class Player{
 public class StaticModifierRunner {
     public static void main(String[] args){
         Player player1 = new Player("maina");
-        System.out.println(player1.getCount());
+        System.out.println(Player.getCount());
         Player player2 = new Player("Fnadel");
-        System.out.println(player1.getCount());
+        System.out.println(Player.getCount());
     }
 }
